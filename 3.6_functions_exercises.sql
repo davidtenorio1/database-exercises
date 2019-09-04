19c50237c6cc9d9d75249bd23621318834ffbc12
davidtenorio1 <dtenorio2169@gmail.com>
2019-09-04 15:22:13 -0500

    updated answers

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -1,8 +1,12 @@
 --Copy the order by exercise and save it as 3.6_functions_exercises.sql.
 --Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
-select concat(first_name, ' ', last_name) as full_name from employees where last_name like 'e%e' order by emp_no;
+select concat(first_name, ' ', last_name) as full_name 
+from employees 
+where concat(first_name, ' ', last_name) like 'e%e' order by emp_no;
 --Convert the names produced in your last query to all uppercase.
-select concat(upper(first_name), ' ', upper(last_name)) as full_name from employees where last_name like 'e%e' order by emp_no
+select upper(concat(first_name, ' ', last_name)) as full_name 
+from employees 
+where concat(first_name, ' ', last_name) like 'e%e' order by emp_no;
 --For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
 select datediff(now(),hire_date) from employees where (hire_date between '1990-01-01' and '1999-12-31') and  birth_date like '%12-25'order by birth_date, hire_date desc;
 --Find the smallest and largest salary from the salaries table.
