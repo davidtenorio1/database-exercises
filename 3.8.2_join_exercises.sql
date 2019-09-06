f0d2e2476d51277b0a24d74245e3d6352106754b
davidtenorio1 <dtenorio2169@gmail.com>
2019-09-06 09:24:36 -0500

    new exercise

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -14,7 +14,11 @@ FROM users
 RIGHT JOIN roles ON users.role_id = roles.id;
 
 --Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. Hint: You will also need to use group by in the query.
-
+SELECT roles.name,COUNT(*)
+FROM roles
+JOIN users
+ON users.role_id=roles.id
+GROUP BY roles.name;
 --Use the employees database.
 use employees;
 
@@ -66,6 +70,37 @@ select distinct departments.dept_name, avg(salaries.salary) as aveage_salary
 from departments 
 join dept_emp on dept_emp.dept_no = departments.dept_no
 join salaries on salaries.emp_no = dept_emp.emp_no
-where dept_emp.to_date = '9999-01-01'
+where dept_emp.to_date = '9999-01-01' and salaries.to_date = '9999-01-01'
 group by dept_name
 order by avg(salaries.salary) desc;
+
+--Who is the highest paid employee in the Marketing department?
+select distinct departments.dept_name, salaries.salary as employee_salary, employees.first_name, employees.last_name
+from departments 
+join dept_emp on dept_emp.dept_no = departments.dept_no
+join salaries on salaries.emp_no = dept_emp.emp_no
+join employees on employees.emp_no = dept_emp.emp_no
+where dept_emp.to_date = '9999-01-01' and salaries.to_date = '9999-01-01' and dept_name = 'Marketing'
+order by salaries.salary desc
+limit 1;
+
+--Which current department manager has the highest salary?
+select departments.dept_name as Department_Name, CONCAT(employees.first_name, ' ', employees.last_name) AS full_name, salaries.salary
+from departments
+join dept_manager on departments.dept_no = dept_manager.dept_no
+join employees on employees.emp_no = dept_manager.emp_no
+join salaries on salaries.emp_no = dept_manager.emp_no
+WHERE dept_manager.to_date = '9999-01-01' and salaries.to_date = '9999-01-01'
+order by salary desc
+limit 1;
+
+--Bonus Find the names of all current employees, their department name, and their current manager's name.
+select CONCAT(employees.first_name, ' ', employees.last_name) as Employee_Name, departments.dept_name as Department_Name, CONCAT(employees.first_name, ' ', employees.last_name) as Manager_Name
+from employees
+join dept_emp on dept_emp.emp_no = employees.emp_no
+join dept_manager on dept_manager.dept_no = dept_emp.dept_no
+join departments on departments.dept_no = dept_manager.dept_no
+where dept_emp.to_date = '9999-01-01' and dept_manager.to_date = '9999-01-01'
+order by Department_Name;
+
+--Bonus Find the highest paid employee in each department.
---
+++ 3.8.3_subqueries_exercises.sql
@@ -0,0 +1,13 @@
+--Create a file named 3.8.3_subqueries_exercises.sql and craft queries to return the results for the following criteria:
+
+--Find all the employees with the same hire date as employee 101010 using a sub-query.
+
+69 Rows
+
+--Find all the titles held by all employees with the first name Aamod.
+
+314 total titles, 6 unique titles
+
+--How many people in the employees table are no longer working for the company?
+
+--Find all the current department managers that are female.
