aad1145293a4aaeda29ca7e25ed89eab7689f16c
davidtenorio1 <dtenorio2169@gmail.com>
2019-09-09 15:43:07 -0500

    updates

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -235,8 +235,10 @@ limit 5;
 --SELECT Statements
 --Select all columns from the actor table.
 select * from actor;
+
 --Select only the last_name column from the actor table.
 select last_name from actor;
+
 --Select only the following columns from the film table.
 select * from film;
 
@@ -245,25 +247,92 @@ select * from film;
 --DISTINCT Operator
 --Select all distinct (different) last names from the actor table.
 select distinct last_name from actor;
+
 --Select all distinct (different) postal codes from the address table.
 select distinct postal_code from address;
+
 --Select all distinct (different) ratings from the film table.
 select distinct rating from film;
 
 
+
 --WHERE Clause
 --Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+select title, description, rating, length
+from film
+where length >= 180;
 
 --Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+select payment_id, amount, payment_date
+from payment
+where payment_date >= '2005-5-27';
 
 --Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+select payment_id, amount, payment_date
+from payment
+where payment_date > '2005-05-27 00:00:00' and payment_date < '2005-05-28 00:00:00';
 
 --Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
+select *
+from customer
+where last_name like "s%" and first_name like "%n";
 
 --Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+select *
+from customer
+where active = '0' and last_name like "m%";
 
 --Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
+select *
+from category
+where (name like "c%" or name like "s%" or name like "t%") and category_id > 4;
 
 --Select all columns minus the password column from the staff table for rows that contain a password.
+select staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
+from staff
+where password is not null;
 
 --Select all columns minus the password column from the staff table for rows that do not contain a password.
+select staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
+from staff
+where password is null;
+
+
+
+--IN Operator
+--Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+select phone, district
+from address
+where district in ('California', 'Taipei', 'England', 'West Java');
+
+--Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+select payment_id, amount, payment_date
+from payment
+where date(payment_date) in ('2005-05-25','2005-05-27','2005-05-29');
+
+--Select all columns from the film table for films rated G, PG-13 or NC-17.
+select *
+from film
+where rating in ('G', 'PG-13', 'NC-17');
+
+
+
+
+--BETWEEN Operator
+--Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.
+select *
+from payment
+where payment_date between '2005-05-25 00:00:00' and '2005-05-25 23:59:59';
+
+--Select the following columns from the film table for films where the length of the description is between 100 and 120.
+--Hint: total_rental_cost = rental_duration * rental_rate
+
+
+
+
+
+
+
+
+
+
